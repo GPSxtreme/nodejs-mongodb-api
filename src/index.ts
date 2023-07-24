f811c35ef35192bc3d79186fb5ad42c0fb5945bc
Prudhvi Suraaj <86603322+GPSxtreme@users.noreply.github.com>
2023-07-24 19:34:36 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { connection } from "./config/db";
 import { app } from "./app";
 import { PORT } from "./config/environmentVariables";
 
-const port: number = parseInt(PORT ?? "8000", 10); // default port to listen
+const port: number = parseInt(PORT ?? process.env.PORT ?? "8000", 10); // default port to listen
 
 app.get("/", (res: Response) => {
   res.send(`Welcome to gpsxtreme's flutter todo app. Hello from server!`);
@@ -15,7 +15,7 @@ connection
   .then(() => {
     app.listen(port, () => {
       console.log(
-        `Welcome to gpsxtreme's flutter todo app\nServer started at http://localhost:${port}`
+        `Welcome to gpsxtreme's flutter todo app\nServer listening at port ${port}`
       );
     });
   })
