a6a1c1f7677caf55a14ab8d6459229db26a48e6d
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-04 01:12:41 +0530

    Update todoRouter.ts

--- src/router/todoRouter.ts
+++ src/router/todoRouter.ts
@@ -1,8 +1,14 @@
 const router = require("express").Router();
-import { addTodo, getTodos, deleteTodo } from "../controller/todoController";
+import {
+  addTodo,
+  getTodos,
+  deleteTodo,
+  updateTodo,
+} from "../controller/todoController";
 import { AuthUtils } from "../utils/authUtils";
 export { router };
 
 router.post("/addTodo", AuthUtils.preAuthChecker, addTodo);
 router.get("/getTodos", AuthUtils.preAuthChecker, getTodos);
 router.get("/deleteTodo", AuthUtils.preAuthChecker, deleteTodo);
+router.post("/updateTodo", AuthUtils.preAuthChecker, updateTodo);
