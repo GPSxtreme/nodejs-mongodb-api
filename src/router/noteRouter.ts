547d07f16035084d458ea18abd429e4abc3cd630
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-04 01:12:35 +0530

    Update noteRouter.ts

--- src/router/noteRouter.ts
+++ src/router/noteRouter.ts
@@ -1,8 +1,14 @@
 const router = require("express").Router();
-import { addNote, getNotes, deleteNote } from "../controller/noteController";
+import {
+  addNote,
+  getNotes,
+  deleteNote,
+  updateNote,
+} from "../controller/noteController";
 import { AuthUtils } from "../utils/authUtils";
 export { router };
 
 router.post("/addNote", AuthUtils.preAuthChecker, addNote);
 router.get("/getNotes", AuthUtils.preAuthChecker, getNotes);
 router.get("/deleteNote", AuthUtils.preAuthChecker, deleteNote);
+router.post("/updateNote", AuthUtils.preAuthChecker, updateNote);
