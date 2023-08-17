53c29c1e84b010f24bdc254f579c3c8755515d7d
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-17 13:39:43 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 import { router as UserRouter } from "./router/userRouter";
 import { router as TodoRouter } from "./router/todoRouter";
 import { router as NoteRouter } from "./router/noteRouter";
@@ -7,6 +8,9 @@ import { router as DataRouter } from "./router/dataRouter";
 export { app };
 
 const app = express();
+console.log(path.join(__dirname, "..", "assets"));
+// Serve static files from the "assets" directory
+app.use("/assets", express.static(path.join(__dirname, "..", "assets/")));
 
 // Use body-parser middleware
 app.use(bodyParser.json());
