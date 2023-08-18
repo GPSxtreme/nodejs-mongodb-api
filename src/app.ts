42b24d9a3410cf3e0f6dcc1d8dab44c24966c4ec
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-19 01:23:45 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
+import { engine } from "express-handlebars"; // Import Handlebars
 import { router as UserRouter } from "./router/userRouter";
 import { router as TodoRouter } from "./router/todoRouter";
 import { router as NoteRouter } from "./router/noteRouter";
@@ -8,9 +9,14 @@ import { router as DataRouter } from "./router/dataRouter";
 export { app };
 
 const app = express();
+// Set up Handlebars view engine
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "..", "views"));
+
 // Serve static files
-app.use("/assets", express.static(path.join(__dirname, "..", "assets/")));
-app.use("/assets", express.static(path.join(__dirname, "..", "templates/")));
+app.use("/templates", express.static(path.join(__dirname, "..", "templates/")));
+app.use(express.static(path.join(__dirname, "..", "public/")));
 // Use body-parser middleware
 app.use(bodyParser.json());
 app.use("/user", UserRouter);
