09aa0b73a57b72a031eca47471cdb181c4408dc2
Prudhvi Suraaj <prudhvisuraaj@gmail.com>
2023-08-23 18:45:55 +0530

    Update userRouter.ts

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-import fs from "fs";
 import {
   register,
   login,
@@ -12,23 +11,10 @@ import {
 } from "../controller/userController";
 import { Response, Request } from "express";
 export { router };
-import {
-  userProfilePictureStorage,
-  uploadUserProfilePicture,
-} from "../middleware/upload";
 import { AuthUtils } from "../utils/authUtils";
-import path from "path";
-
-userProfilePictureStorage;
 
 router.post("/registration", register);
 router.post("/login", login);
-router.post(
-  "/uploadProfilePicture",
-  AuthUtils.preAuthChecker,
-  uploadUserProfilePicture.single("profilePicture"),
-  uploadProfilePicture
-);
 router.post("/updateUserData", AuthUtils.preAuthChecker, updateUserData);
 router.get("/verify/:id", verifyUserEmail);
 router.get(
